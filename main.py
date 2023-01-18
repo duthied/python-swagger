d3b81ea1d4cc22bbd1ec5ac91a0e5aef142c8065
Devlon <devlon@Devlons-MacBook-Pro.local>
2023-01-17 22:48:01 -0500

    Adds templates

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
 # main.py
-from flask import Flask, request
+from flask import Flask
+from blueprints.basic_endpoints import blueprint as basic_endpoint
+from blueprints.jinja_endpoint import blueprint as jinja_template_blueprint
+
 app = Flask(__name__)
+app.register_blueprint(basic_endpoint)
+app.register_blueprint(jinja_template_blueprint)
 
-@app.route('/basic_api/entities', methods=['GET', 'POST'])
-def entities():
-  if request.method == "GET":
-    return {
-      'message': 'This endpoint should return a list of entities',
-      'method': request.method
-    }
-  if request.method == "POST":
-    return {
-      'message': 'This endpoint should create an entity',
-      'method': request.method,
-      'body': request.json
-    }
-
-@app.route('/basic_api/entities/<int:entity_id>', methods=['GET', 'PUT', 'DELETE'])
-def entity(entity_id):
-  if request.method == "GET":
-    return {
-      'id': entity_id,
-      'message': 'This endpoint should return the entity {} details'.format(entity_id),
-      'method': request.method
-    }
-  if request.method == "PUT":
-    return {
-      'id': entity_id,
-      'message': 'This endpoint should update the entity {}'.format(entity_id),
-      'method': request.method,
-      'body': request.json
-    }
-  if request.method == "DELETE":
-    return {
-      'id': entity_id,
-      'message': 'This endpoint should delete the entity {}'.format(entity_id),
-      'method': request.method
-    }
+if __name__ == "__main__":
+  app.run()
